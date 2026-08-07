fa6f77d16574bf45434431c2fd60fcd34e9a0c8f
Dmitry Belyavskiy <beldmit@gmail.com>
2026-08-07 17:25:07 +0200

    openssh-6.7p1-coverity

--- misc.c
+++ misc.c
@@ -1612,6 +1612,8 @@ sanitise_stdfd(void)
 	}
 	if (nullfd > STDERR_FILENO)
 		close(nullfd);
+	/* coverity[leaked_handle : FALSE]*/
+	/* coverity[leaked_handle : FALSE]*/
 }
 
 char *
@@ -2815,6 +2817,7 @@ stdfd_devnull(int do_stdin, int do_stdout, int do_stderr)
 	}
 	if (devnull > STDERR_FILENO)
 		close(devnull);
+	/* coverity[leaked_handle : FALSE]*/
 	return ret;
 }
 
